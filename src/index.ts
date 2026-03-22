dfddd093eda91050fddad774118d3d12e0cfa639
Edy Cu <edy.cu@live.com>
2026-03-22 14:59:57 +0700

    fix: default port fallback to 3050 for consistency

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 import express from "express";
 
 const app = express();
-const port = process.env["PORT"] || 5000;
+const port = process.env["PORT"] || 3050;
 
 app.use(cors());
 app.use(express.json());
